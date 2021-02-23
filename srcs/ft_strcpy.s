5bdf1484c27f56e7c5f2d61ff8a252fe11f83ef6
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2021-02-23 17:54:03 +0100

    ft_strlcpy.s

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -1,4 +1,18 @@
 section .text
-    global _ft_strcpy
+	global _ft_strcpy
 
-_ft_strcpy:					; char	*ft_strcpy(char *dst, const char *s);
+_ft_strcpy:							; char	*ft_strcpy(char *dst, const char *s);
+	xor		rcx, rcx				; set counter to 0
+
+_loop:
+	cmp		BYTE [rsi + rcx], 0		; check *(src + rcx) != 0
+	je		_return					; if so, goto _return
+	mov		dl, BYTE [rsi + rcx]	; if not, copy chaaracter in dest into dl
+	mov		BYTE [rdi + rcx], dl	; copy character in dl into src
+	inc		rcx						; increment counter
+	jmp		_loop					; repeat loop
+
+_return:
+	mov		BYTE [rdi, rcx], 0		; put '\0' at end of dest
+	mov		rax, rdi				; put dest in rax
+	ret								; return rax
--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -4,11 +4,11 @@ section .text
 _ft_strlen:					; size_t	ft_strlen(const char *str);
 	xor rax, rax			; set return value (rax) to 0
 
-_count:
+_loop:
 	cmp BYTE [rdi + rax], 0	; check if *(str + rax) == 0 (rdi is the first parameter)
 	je	_return				; if *(str + rax) == 0 goto _return
 	inc rax					; if not, increment rax;
-	jmp _count				; repeat _count
+	jmp _loop				; repeat _loop
 
 _return:
 	ret						; return of function (rax)
