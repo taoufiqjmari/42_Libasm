8cb2c7762e4d957435b307261ce12044b96b44f8
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2021-02-23 18:58:55 +0100

    ft_strdup

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -1,4 +1,21 @@
 section .text
-    global _ft_strdup
+	global  _ft_strdup
+	extern  _ft_strlen
+	extern  _ft_strcpy
+	extern  _malloc
 
 _ft_strdup:					; char	*ft_strdup(const char *s1);
+	call	_ft_strlen		; call ft_strlen
+	inc		rax				; increment rax for '\0'
+	push	rdi				; save original string
+	mov		rdi, rax		; set parameter for malloc
+	call	_malloc			; call malloc
+	cmp		rax, 0			; check if malloc failed
+	je		_error			; if so, goto _error
+	mov		rdi, rax		; set first parameter for ft_strcpy
+	pop		rsi				; set second parameters for ft_strcpy
+	call	_ft_strcpy		; call ft_strcpy
+	ret						; return duplicated string
+
+_error:						;
+	ret						; return 0 in case malloc failed
